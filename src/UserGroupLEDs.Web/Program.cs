5c62ad3b40c09778deaac42b3c85b66d88aeca4a
Michael Stokesbary <mike@stokesbary.me>
2021-11-17 07:04:56 +0000

    Got yarp configured and exposing web

--- src/UserGroupLEDs.Web/Program.cs
+++ src/UserGroupLEDs.Web/Program.cs
@@ -1,6 +1,8 @@
+using System.Runtime.InteropServices;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => $"Hello World from {RuntimeInformation.FrameworkDescription} on {RuntimeInformation.OSDescription}!");
 
 app.Run();

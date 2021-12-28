e9d164c6e121f7382ee9971e3c334ca5f2e2d192
Michael Stokesbary <mike@stokesbary.me>
2021-12-28 22:03:20 +0000

    added blazor wasm and tye

--- src/UserGroupLEDs.Web/Program.cs
+++ src/UserGroupLEDs.Web/Program.cs
@@ -1,8 +1,21 @@
-using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using UserGroupLEDs.Web;
+using UserGroupLEDs.Web.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-app.MapGet("/", () => $"UserGroupLEDS.Web running on {RuntimeInformation.FrameworkDescription} on {RuntimeInformation.OSDescription}!");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<WeatherClient>(client =>
+    {
+         //client.BaseAddress = builder.Configuration.GetServiceUri("usergroupleds-api");
+         //builder.Configuration.Bind("apiHost", )
+         Console.WriteLine(builder.Configuration["apiHost"]);
+         client.BaseAddress = new Uri(builder.Configuration["apiHost"]);
+    });
 
-app.Run();
+Console.WriteLine("Stokes: just a test");
+
+await builder.Build().RunAsync();
